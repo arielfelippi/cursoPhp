f708e3bbfacfaf58c25af03aae62706f90202a71
Ariel Felippi <ariel.felippi@hotmail.com>
2021-04-13 20:09:28 -0300

    13-04-2021: novo branch infoserv

--- aula1.php
+++ aula1.php
@@ -8,3 +8,10 @@ phpinfo();
 echo "simulando um conflito";
 
 echo " criamos o branch DEV";
+/*
+O comando abaixo imprime a data atual
+echo - imprimir
+date - data atual
+"d/m/Y" - formato da data
+*/
+echo date("d/m/Y"); // 13/04/2021
