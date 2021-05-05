d3bb7e8db54a986e1f9a5598b6d00990ccfa5d8e
Ariel Felippi <ariel.felippi@hotmail.com>
2021-05-05 02:31:02 -0300

    criacao de rotas e estruturacao do projeto e arquivos

--- Configuration.php
+++ Configuration.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
-require __DIR__ . "/App/src/core/Autoloader.php";
+require __DIR__ . "/app/src/core/autoloader/Autoloader.php";
 
 session_start();
 
